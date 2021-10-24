6fa732f2e0cf5e9f5517905dbf374548e485a236
sn77sohan <sohan.ahmedself@gmail.com>
2021-10-24 20:36:55 +0600

    third-app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>This is third react-js app</h2>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos amet quaerat dolore reiciendis voluptatum commodi, temporibus non optio, similique fugit? Dolorem reprehenderit iste non aperiam quaerat nihil, omnis eum, labore sit reiciendis tempora dolore animi ea? Numquam laboriosam placeat deleniti, eum beatae doloribus eligendi voluptatibus officia, doloremque neque commodi excepturi, quia nulla ex exercitationem? Harum accusamus autem excepturi adipisci odit omnis doloribus quidem porro quos, amet optio animi error pariatur esse tenetur beatae illum eaque recusandae quis ea. Omnis, rem, maxime cupiditate eveniet nobis doloribus iure, possimus temporibus numquam enim tempora! Maiores cumque deleniti eligendi esse, fugit earum corporis.</p>
     </div>
   );
 }
